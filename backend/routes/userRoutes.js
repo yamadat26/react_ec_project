3cae84a6dd68be5a4402cdb4154c297acae51fa2
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2021-04-21 22:03:16 +0900

    Login & Register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, resisterUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, resisterUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddkeware.js'
 
 router.route('/').post(resisterUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,24 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { cartRedeucer } from './reducers/cartRedeucers'
+import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartRedeucer
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage }
+  cart: { cartItems: cartItemsFromLocalStorage },
+  userLogin: { userInfo: userInfoFromLocalStorage}
 }
 
 const middleware = [thunk]
