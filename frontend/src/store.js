52cec7d5a3c462b9586fbb580eb96f7edbb536f1
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2021-04-25 12:49:40 +0900

    create shipping screen and payment screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,8 +19,13 @@ const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse
 
 const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAdressFromLocalStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage },
+  cart: {
+    cartItems: cartItemsFromLocalStorage,
+    shippingAddress:  shippingAdressFromLocalStorage
+  },
   userLogin: { userInfo: userInfoFromLocalStorage}
 }
 
