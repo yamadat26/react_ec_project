63c7dd55d8dad7b664e1f5c66949d4323d44a6da
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2021-05-10 11:13:02 +0900

    Add image upload function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById } from '../controllers/productContoroller.js'
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct
+} from '../controllers/productContoroller.js'
+import { protect, admin } from '../middleware/authMiddkeware.js'
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
-router.route('/:id').get(getProductById)
+
 
 export default router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import PlaceOrderScreeen from './screens/PlaceOrderScreen'
 import OrderScreeen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -33,6 +35,8 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productList' component={ProductListScreen} />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
